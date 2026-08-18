08560d39e3c7d2344420c9fb9c84d8fddf028183
Kandarp Sharma <sharma.kandarp24@gmail.com>
2026-08-18 21:19:17 +0530

    feat: Add host authentication

--- app/controllers/pg_health/application_controller.rb
+++ app/controllers/pg_health/application_controller.rb
@@ -1,4 +1,6 @@
 module PgHealth
   class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    layout "pg_health/application"
   end
 end
--- lib/pg_health/engine.rb
+++ lib/pg_health/engine.rb
@@ -1,5 +1,10 @@
 module PgHealth
   class Engine < ::Rails::Engine
     isolate_namespace PgHealth
+
+    # Enables middleware configuration: PgHealth::Engine.use(...)
+    def self.use(*args, &block)
+      middleware.use(*args, &block)
+    end
   end
 end
